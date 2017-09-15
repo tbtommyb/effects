326efce9241ad029d11dbf9631300d09ba224790
Thomas Barrett <thomas.barrett@yourgolftravel.com>
2017-09-15 17:52:11 +0100

    Added support for buttons. Added documentation to describe protocol

--- arduino/pots.c
+++ arduino/pots.c
@@ -1,32 +1,89 @@
-// Read in potentiometer value
-// Code based on this:
-// https://github.com/hexagon5un/AVR-Programming/blob/master/Chapter07_Analog-to-Digital-Conversion-I/nightLight/
+/*
+Read in potentiometer value
+Code based on this:
+https://github.com/hexagon5un/AVR-Programming/blob/master/Chapter07_Analog-to-Digital-Conversion-I/nightLight/
 
-// TODO: add support for buttons to toggle controls
+Output is in the following 16 bit format:
+
+0000 00 0000000000
+\    \  \
+ \    \  10 bit ADC value
+  \    01 indicates ON, ten lowest bits will be latest ADC value
+   \   00 indicates OFF, ten lowest bits will be empty
+    Four bit control identifier, start from 0000
+
+There will be no output until a control is turned on. It will not output again
+until its potentiometer value changes or it is turned off:
+
+0011010000000000 <- ctrl 3 is ON and has value 0
+0011010000000111 <- ctrl 3 is ON and has value 7
+0011000000000000 <- ctrl 3 is OFF. Output from ctrl 3 will now stop
+0011011111111111 <- ctrl 3 is ON and has value 0x3FF
+
+*/
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
 #include "USART.h"
+#include "pots.h"
 
-#define ADC0 PINC0
-#define ADC1 PINC1
-#define ADC2 PINC2
-#define ADC3 PINC3
+const int POT_PINS[NUM_CTRLS] = { PINC0, PINC1 };
+const int BUTTON_PINS[NUM_CTRLS] = { PIND0, PIND1 };
 
-#define lowByte(w) ((uint8_t) ((w) & 0xff))
-#define highByte(w) ((uint8_t) ((w) >> 8))
+struct _Button {
+  uint8_t pin;
+  bool is_pressed;
+};
 
-typedef struct {
+struct _Pot {
   uint8_t pin;
   uint16_t val;
-} Pot;
+};
 
-Pot newPot(uint8_t pin) {
+struct _Control {
+  Button btn;
   Pot pot;
-  pot.pin = pin;
-  pot.val = 0;
-  return pot;
+  bool is_on;
+  uint16_t id;
+};
+
+Control newControl(uint16_t i) {
+  Button btn = {
+    .pin = BUTTON_PINS[i],
+    .is_pressed = false
+  };
+
+  Pot pot = {
+    .pin = POT_PINS[i],
+    .val = 0
+  };
+
+  Control ctrl = {
+    .btn = btn,
+    .pot = pot,
+    .is_on = false,
+    .id = i
+  };
+
+  return ctrl;
 }
+
+uint16_t pollPot(Pot pot) {
+  return readADC(pot.pin);
+}
+
+bool isPressed(Button btn) {
+  if (debounce(btn.pin)) {
+      // delay 5ms
+    if (debounce(btn.pin)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// ADC
 
 void initADC(void) {
   ADMUX |= (1 << REFS0);                 // reference voltage on AVCC
@@ -41,28 +98,77 @@ uint16_t readADC(uint8_t channel) {
   return (ADC);
 }
 
-void pollPot(Pot pot) {
-  uint16_t val = readADC(pot.pin);
-  if (val != pot.val) {
-    pot.val = val;
-    uint16_t output = pot.val | (pot.pin << 12); // move pin number up to top four bits
-    transmitByte(lowByte(output));               // USART transmits LSB -> MSB
-    transmitByte(highByte(output));
+// Utilities
+
+uint8_t debounce(uint8_t pin) {
+  if (bit_is_clear(BUTTON_PIN, pin)) {      // button is pressed now
+    _delay_us(DEBOUNCE_TIME);
+    if (bit_is_clear(BUTTON_PIN, pin)) {    // still pressed
+      return (1);
+    }
   }
+  return 0;
+}
+
+void transmitCtrlValue(Control ctrl) {
+  // Submit ctrl ID in top four, ON bit set and pot value in bottom ten
+  uint16_t output = ID_TAG(ctrl.id) | (1 << CTRL_ON_BIT) | ctrl.pot.val;
+  transmitWord(output);
+}
+
+void transmitCtrlOff(Control ctrl) {
+  // Simply submit ctrl ID top four bits, rest empty
+  uint16_t output = ID_TAG(ctrl.id) | CTRL_OFF;
+  transmitWord(output);
+}
+
+void transmitWord(uint16_t value) {
+  transmitByte(LOW_BYTE(value)); // USART transmits LSB -> MSB
+  transmitByte(HIGH_BYTE(value));
 }
 
 int main(void) {
   // -------- Inits --------- //
-  Pot ADC0_pot = newPot(ADC0);
-  Pot ADC1_pot = newPot(ADC1);
+  Control controls[NUM_CTRLS];
+  for (uint16_t i = 0; i < NUM_CTRLS; i++) {
+    Control ctrl = newControl(i);
+    controls[i] = ctrl;
+    BUTTON_PORT |= (1 << ctrl.btn.pin); // enable pull-up resistor on button pin
+  }
 
   initADC();
 
   // -------- Main loop -------- //
-  while(1) {
-    pollPot(ADC0_pot);
-    pollPot(ADC1_pot);
+  while (1) {
+    for (uint16_t i = 0; i < NUM_CTRLS; i++) {
+      Control ctrl = controls[i];
 
+      uint16_t currentPotValue = pollPot(ctrl.pot);
+      if (currentPotValue != ctrl.pot.val) {
+      // Record new pot value
+        ctrl.pot.val = currentPotValue;
+        if (ctrl.is_on) {
+          transmitCtrlValue(ctrl);
+        }
+      }
+
+      if (isPressed (ctrl.btn)) {
+        if (ctrl.btn.is_pressed == false) {
+          // Button wasn't pressed last time so toggle ctrl and transmit change
+          ctrl.btn.is_pressed = true;
+          ctrl.is_on = !ctrl.is_on;
+          if (ctrl.is_on) {
+            transmitCtrlValue(ctrl);
+          } else {
+            transmitCtrlOff(ctrl);
+          }
+        }
+        // Button is being held down so do nothing here
+      } else {
+        // To handle when the button has been let go
+        ctrl.btn.is_pressed = false;
+      }
+    }
     _delay_ms(20);
   }
   return 0;
