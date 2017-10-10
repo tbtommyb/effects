96ecca5c4fb10aec1f32576f1d01de81d20fc048
Tom Johnson <tom@tmjohnson.co.uk>
2017-10-10 13:33:06 +0000

    Removed unnecessary methods

--- Source/Control.cpp
+++ Source/Control.cpp
@@ -16,24 +16,11 @@ Control::Control(int id) : val(0),
   this->id = id;
 };
 
-Control::Control(const Control& other) {
-  id = other.id;
-  val = other.val;
-  isOn = other.isOn;
-};
-
-Control::~Control() { };
+Control::~Control() = default;
 
 void Control::update(int val, bool isOn) {
   this->val = val;
   this->isOn = isOn;
 
   sendChangeMessage();
-};
-
-Control& Control::operator=(Control rhs) {
-  id = rhs.id;
-  val = rhs.val;
-  isOn = rhs.isOn;
-  return *this;
 };
